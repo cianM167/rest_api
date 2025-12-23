4a1b539eec9854e9015afefad9d8b4d16423e8a3
cianM167 <cianmcguire167@gmail.com>
2025-12-23 00:12:14 +0000

    Trying to create stateful protocol for storing gamestate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use actix_web::{web, App, HttpServer, Responder};
+use std::collections::HashMap;
 
 mod models;
 mod handlers;
+
+struct Character {
+    str: i8,
+    dex: i8,
+    con: i8,
+    int: i8,
+    wiz: i8,
+    chr: i8,   
+}
+
+struct Game {
+    characters: HashMap<String, Character>,
+}
 
 async fn hello() -> impl Responder {
     "<h1>Hello World!!!</h1>"
@@ -9,6 +23,9 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut active_games: HashMap<String, Game> = HashMap::new();//hasmap containing all active games
+
+
     HttpServer::new(|| {
         App::new()
             .service(
@@ -17,8 +34,20 @@ async fn main() -> std::io::Result<()> {
                     .route("/items", web::get().to(handlers::get_items))
                     .route("/items/{id}", web::get().to(handlers::get_item))
             )
+            .service(
+                web::scope("/game")
+                    .wrap(Timeout::new(Duration::from_secs(5)))
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
+
+///*
+
+async fn add_game(db_parameters) {
+
+}
+
+//*/
