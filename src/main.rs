b5004fa5087981d3470bbc7c20e51181e27a237d
cianM167 <cianmcguire167@gmail.com>
2025-12-23 01:38:08 +0000

    Creating temp connections for game sessions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use actix_web::{web, App, HttpServer, Responder};
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::{Arc, RwLock}, time::Instant};
+use tokio::sync::Notify;
 
 mod models;
 mod handlers;
+
+type Connections = RwLock<HashMap<String, TempConnection>>;
 
 struct Character {
     str: i8,
@@ -17,14 +20,31 @@ struct Game {
     characters: HashMap<String, Character>,
 }
 
+struct TempConnection {
+    last_activity: Instant,
+}
+
 async fn hello() -> impl Responder {
     "<h1>Hello World!!!</h1>"
 }
 
+async fn create_connection(
+    id: String,
+    connections: &Connections,
+) {
+   connections.write().unwrap().insert(//explicitly handle this even if it cant happen
+    id,
+    TempConnection {
+        last_activity: Instant::now(), 
+    }
+    );
+
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut active_games: HashMap<String, Game> = HashMap::new();//hasmap containing all active games
-
+    //let mut active_games: HashMap<String, Game> = HashMap::new();//hasmap containing all active games
+    let connections: Connections = RwLock::new(HashMap::new());
 
     HttpServer::new(|| {
         App::new()
@@ -36,7 +56,7 @@ async fn main() -> std::io::Result<()> {
             )
             .service(
                 web::scope("/game")
-                    .wrap(Timeout::new(Duration::from_secs(5)))
+
             )
     })
     .bind("127.0.0.1:8080")?
@@ -44,10 +64,3 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-///*
-
-async fn add_game(db_parameters) {
-
-}
-
-//*/
